db71b7de80b1cd1bfb96f1a41ff656ff7af5523e
DevJuanDuarte <juan.devcontact@gmail.com>
2022-10-17 13:39:46 -0500

    Services

--- urls.py
+++ urls.py
@@ -14,12 +14,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from     django.urls import path, include
+from django.urls import path, include
+from django.conf import settings
+
 
 urlpatterns = [
 
     #APP CORE
     path('', include('core.urls')),
-
+    #APP SERVICES
+    path('services/', include('services.urls')),
+    #APP ADMIN
     path('admin/', admin.site.urls),
 ]
+
+
+if settings.DEBUG:
+    from django.conf.urls.static import static
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
